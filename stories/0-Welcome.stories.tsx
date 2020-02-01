8fe267e6307f44d999b83b8961592182881906d0
Adam Bergman <adam@fransvilhelm.com>
2020-02-01 12:36:52 +0100

    chore: Clean up project

--- stories/0-Welcome.stories.tsx
+++ stories/0-Welcome.stories.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Thing } from '@';
 
 export default {
   title: 'Welcome',
 };
 
-export const toStorybook = () => <Thing />;
+export const toStorybook = () => <p />;
 
 toStorybook.story = {
   name: 'to Storybook',
